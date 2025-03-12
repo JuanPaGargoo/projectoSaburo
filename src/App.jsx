89bebd290e1147ce53f5510c8268af07249ec6a9
Juan Pablo <juanpablogargoo@gmail.com>
2025-03-11 18:11:12 -0600

    Primer Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import NavbarSaburo from './components/NavbarSaburo'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <NavbarSaburo/> 
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ShopMenu.jsx
@@ -0,0 +1,42 @@
+import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button} from "@heroui/react";
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
+
+export default function ShopMenu() {
+  const items = [
+    {
+      key: "new",
+      label: "New file",
+    },
+    {
+      key: "copy",
+      label: "Copy link",
+    },
+    {
+      key: "edit",
+      label: "Edit file",
+    },
+    {
+      key: "delete",
+      label: "Delete file",
+    },
+  ];
+
+  return (
+    <Dropdown>
+      <DropdownTrigger>
+        <Button className="bg-transparent p-0 " endContent={<ChevronDownIcon/>}>Shop</Button>
+      </DropdownTrigger>
+      <DropdownMenu aria-label="Dynamic Actions" items={items}>
+        {(item) => (
+          <DropdownItem
+            key={item.key}
+            className={item.key === "delete" ? "text-danger" : ""}
+            color={item.key === "delete" ? "danger" : "default"}
+          >
+            {item.label}
+          </DropdownItem>
+        )}
+      </DropdownMenu>
+    </Dropdown>
+  );
+}
